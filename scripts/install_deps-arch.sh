103f9955fb1cbd90503bf232f6f1859d0a532433
Jack Lagas <jacklagasw@gmail.com>
2024-07-08 09:46:58 -0700

    Install script

---
+++ scripts/install_deps-arch.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if ! command -v yay &> /dev/null; then
+    echo "yay not found!"
+    read -p "Install yay? [Y/n]: " should_install
+    if [ "$should_install" = "y" ] || [ "$should_install" = "Y" ] || [ "$should_install" = "" ]; then
+        echo "Installing build dependencies"
+        sudo pacman -S --needed --noconfirm git
+        echo "Downloading yay..."
+        git clone https://aur.archlinux.org/yay.git &>/dev/null
+        echo "Installing yay..."
+        cd yay
+        makepkg -si --noconfirm --needed &>/dev/null
+        cd ..
+        rm -rf yay &>/dev/null
+        echo "Finished installing yay"
+    else
+        echo "Exiting..."
+        exit
+    fi
+fi
+
+yay -S --noconfirm --needed oh-my-posh neovim tmux zsh fzf zoxide ripgrep fd
+mkdir -p $HOME/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm    
